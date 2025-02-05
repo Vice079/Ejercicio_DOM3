93d0e468cde7b54e4db05a66eba26d16426c2fb9
Vice079 <v_llorens@hotmail.com>
2025-02-05 13:28:43 +0100

    cambios estilo

--- scrypt.js
+++ scrypt.js
@@ -1,7 +1,23 @@
-console.log("hola")
-const boton = document.getElementById("valor")
-function agregar () {
-console.log(valor)
+const btnAgregar = document.getElementById("btnAgregar");
+const btnSumatoria = document.getElementById("btnSumatoria");
+
+let suma = 0;
+
+function agregar() {
+  const inputValue = document.getElementById("inputNumero").value;
+
+  const listaUl = document.getElementById("lista");
+  const elementoLista = document.createElement("li");
+  elementoLista.innerText = "El numero es: " + inputValue;
+  listaUl.appendChild(elementoLista);
+
+  suma += Number(inputValue);
 }
 
-boton.addEventListener("click", agregar )
+function sumatoria() {
+  const titulo = document.getElementById("titulo");
+  titulo.innerText = "La sumatoria es: " + suma;
+}
+
+btnAgregar.addEventListener("click", agregar);
+btnSumatoria.addEventListener("click", sumatoria);
